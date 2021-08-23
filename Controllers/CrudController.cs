294090ef293e29f7b8789f97748b1cf4ffdb29c1
kavirajan S <kavirajanvaf07072021@gmail.com>
2021-08-23 10:30:00 +0530

    Add project files.

---
+++ Controllers/CrudController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleCrudOperation.Models;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SimpleCrudOperation.Controllers
+{
+    public class CrudController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
